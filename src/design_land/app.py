2b1677d62464309b0fab6e62adf21b4b974722ca
Pavel Ashikhmin <p.ashikhmin@rambler-co.ru>
2020-05-22 02:52:48 +0300

    sclo ready

--- src/design_land/app.py
+++ src/design_land/app.py
@@ -1,12 +1,19 @@
+import json
+
 import waitress
 import flask
 
 app = flask.Flask(__name__, static_url_path="/static", static_folder="static")
 
 
-@app.route("/")
+@app.route("/sclo")
 def index():
-    return flask.render_template("index.html")
+    return flask.render_template("index.html", clothes=form_clothes_dict())
+
+
+def form_clothes_dict():
+    with open(app.static_folder + '/item_category_config.json') as config:
+        return json.loads(config.read())
 
 
 if __name__ == '__main__':
