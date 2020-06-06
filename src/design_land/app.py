f82a45b1d1bf64bad717352e30e9b7356d30696e
Pavel Ashikhmin <p.ashikhmin@rambler-co.ru>
2020-06-06 14:26:58 +0300

    ab slides

--- src/design_land/app.py
+++ src/design_land/app.py
@@ -8,8 +8,18 @@ app = flask.Flask(__name__, static_url_path="/static", static_folder="static")
 
 
 @app.route("/sclo")
+@app.route("/sclo")
 def index():
-    return flask.render_template("index.html", clothes=form_clothes_dict())
+    img_id = int(flask.request.args.get('slide_id', 0))
+    imgs = [
+        '/static/img/slide/Mask_Group%s.png' % (i if i > 1 else '') for i in range(1,5)
+    ]
+    return flask.render_template("index.html", slide_img=imgs[img_id], clothes=form_clothes_dict())
+
+
+@app.route("/daily_task")
+def daily():
+    return flask.render_template("daily-task.html")
 
 
 def form_clothes_dict():
